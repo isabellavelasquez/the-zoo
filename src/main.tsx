643966bcdafe4c79fd1d19049c55b8c7b416ce72
Isabella Velasquez <100156325+isabellavelasquez@users.noreply.github.com>
2025-08-20 14:29:45 +0200

    deploying for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ZooApp } from "./ZooApp";
-import { AnimalCard } from "./components/AnimalCard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
